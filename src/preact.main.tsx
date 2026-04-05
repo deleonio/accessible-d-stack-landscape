f4a37dfaced15728dd32da195ed5d3b6dfe5936d
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-04-05 08:15:33 +0000

    Use @public-ui/theme-default as active theme

--- src/preact.main.tsx
+++ src/preact.main.tsx
@@ -1,11 +1,11 @@
 import { register } from '@public-ui/components';
 import { defineCustomElements } from '@public-ui/components/loader';
-import { KERN_V2 } from '@public-ui/theme-kern';
+import { DEFAULT } from '@public-ui/theme-default';
 
 import { render } from 'preact';
 import App from './App';
 
-register(KERN_V2, defineCustomElements, { translation: { name: 'de' } })
+register(DEFAULT, defineCustomElements, { translation: { name: 'de' } })
 	.then(() => {
 		const htmlElement: HTMLElement | null = document.querySelector<HTMLDivElement>('div#app');
 		if (htmlElement instanceof HTMLElement) {
