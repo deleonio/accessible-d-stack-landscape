02c3b033071ca8df073db1713e645c6b7ba08902
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-04-09 11:23:38 +0200

    format

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -52,7 +52,7 @@ export function FilterBar({
 	];
 
 	return (
-    <section className="filter-bar px-3 md:px-4 lg:px-5 py-2 md:py-3" aria-label={t('search.regionAria')}>
+		<section className="filter-bar px-3 md:px-4 lg:px-5 py-2 md:py-3" aria-label={t('search.regionAria')}>
 			<div className="filter-bar__inner px-3 md:px-4 lg:px-5">
 				<KolSingleSelect
 					className="filter-bar__select filter-bar__select--stack sort-select"
